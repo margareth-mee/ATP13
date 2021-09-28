948e60acb6b0e33db9e413c55389a3db83ce8e9d
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-09-28 16:20:11 -0300

    Menu feito e testado

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,9 +1,17 @@
 public class Calculadora {
     public static void main(String[] args) {
         cabecalho();
+        menu();
     }
 
     static void cabecalho(){
         System.out.println("****** CALCULADORA ******");
+    }
+
+    static void menu(){
+        System.out.println("1 - Somar");
+        System.out.println("2 - Subtrair");
+        System.out.println("3 - Multiplicar");
+        System.out.println("4 - Dividir");
     }
 }
